c846a163321e7eb05a8610f6ba052021d82a4187
Wingspear <ethan.soo@hotmail.com>
2017-10-16 16:03:56 -0700

    Constructed basic menu page! Added icons as well

--- app/src/main/java/com/example/ninja/cooking/MainActivity.java
+++ app/src/main/java/com/example/ninja/cooking/MainActivity.java
@@ -10,18 +10,22 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textRecipes;
+    private TextView textSearch;
+    private TextView textInput;
+    private TextView textCreate;
     private TextView textFavorites;
-    private TextView textSchedules;
-    private TextView textMusic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textRecipes = (TextView) findViewById(R.id.text_recipes);
+        textSearch = (TextView) findViewById(R.id.text_search);
+        textInput = (TextView) findViewById(R.id.text_input);
+        textCreate = (TextView) findViewById(R.id.text_create);
         textFavorites = (TextView) findViewById(R.id.text_favorites);
-        textSchedules = (TextView) findViewById(R.id.text_schedules);
-        textMusic = (TextView) findViewById(R.id.text_music);
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView)
                 findViewById(R.id.bottom_navigation);
@@ -31,21 +35,40 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                         switch (item.getItemId()) {
+                            case R.id.action_recipeDatabase:
+                                textRecipes.setVisibility(View.VISIBLE);
+                                textSearch.setVisibility(View.GONE);
+                                textInput.setVisibility(View.GONE);
+                                textCreate.setVisibility(View.GONE);
+                                textFavorites.setVisibility(View.GONE);
+                                break;
+                            case R.id.action_searchDatabase:
+                                textRecipes.setVisibility(View.GONE);
+                                textSearch.setVisibility(View.VISIBLE);
+                                textInput.setVisibility(View.GONE);
+                                textCreate.setVisibility(View.GONE);
+                                textFavorites.setVisibility(View.GONE);
+                                break;
+                            case R.id.action_input:
+                                textRecipes.setVisibility(View.GONE);
+                                textSearch.setVisibility(View.GONE);
+                                textInput.setVisibility(View.VISIBLE);
+                                textCreate.setVisibility(View.GONE);
+                                textFavorites.setVisibility(View.GONE);
+                                break;
+                            case R.id.action_create:
+                                textRecipes.setVisibility(View.GONE);
+                                textSearch.setVisibility(View.GONE);
+                                textInput.setVisibility(View.GONE);
+                                textCreate.setVisibility(View.VISIBLE);
+                                textFavorites.setVisibility(View.GONE);
+                                break;
                             case R.id.action_favorites:
+                                textRecipes.setVisibility(View.GONE);
+                                textSearch.setVisibility(View.GONE);
+                                textInput.setVisibility(View.GONE);
+                                textCreate.setVisibility(View.GONE);
                                 textFavorites.setVisibility(View.VISIBLE);
-                                textSchedules.setVisibility(View.GONE);
-                                textMusic.setVisibility(View.GONE);
-                                break;
-                            case R.id.action_schedules:
-                                textFavorites.setVisibility(View.GONE);
-                                textSchedules.setVisibility(View.VISIBLE);
-                                textMusic.setVisibility(View.GONE);
-                                break;
-                            case R.id.action_music:
-                                textFavorites.setVisibility(View.GONE);
-                                textSchedules.setVisibility(View.GONE);
-                                textMusic.setVisibility(View.VISIBLE);
-                                break;
                         }
                         return false;
                     }
